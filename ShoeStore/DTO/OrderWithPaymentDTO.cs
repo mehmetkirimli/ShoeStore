8f09808bbfeabca045a9299a2e154f374fc92977
mehmet kirimli <kirimlimehmet@gmail.comm>
2025-01-20 18:12:05 +0300

    fix payment with order

---
+++ ShoeStore/DTO/OrderWithPaymentDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace ShoeStore.DTO
+{
+    public class OrderWithPaymentDTO
+    {
+        public int UserId { get; set; }  // Siparişi oluşturan kullanıcı
+        public OrderDTO orderDto { get; set; }  // Sipariş bilgileri
+        public PaymentDTO paymentDto { get; set; }  // Ödeme bilgileri
+    }
+}
